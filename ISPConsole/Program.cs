632ddc5c089c44d9bebbfe910a4fbd05abe060c8
Abhinav Sushanth Ganji <abhinav.sushanth@gmail.com>
2021-11-02 13:24:20 -0700

    ISP Code

---
+++ ISPConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ISPLibrary;
+
+namespace ISPConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IBorrowableDVD dvd = new DVD();
+        }
+    }
+}
---
+++ ISPLibrary/GeneralInterface/ILibraryItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISPLibrary
+{
+    public interface ILibraryItem
+    {
+        string LibraryId { get; set; }
+        string Title { get; set; }
+        //string Author { get; set; }
+        //DateTime BorrowDate { get; set; }
+        //string Borrower { get; set; }
+        //int CheckoutDurationInDays { get; set; }        
+        //int Pages { get; set; }
+        //void CheckIn();
+        //void CheckOut(string borrower);
+        //DateTime GetDueDate();
+    }
+}
